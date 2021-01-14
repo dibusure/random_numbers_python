ec5e0823fc68af1676fae055cfc82abb91218963
dibusure <69085213+dibusure@users.noreply.github.com>
2021-01-14 22:08:33 +0300

    new version! 1.1! Happy!
    Add:
    Green and blue background for numbers
    New input for border random number
    Update codename: "Green numbers is beatiful!".
    update 1.1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# 1.1 UPDATE name: Green numbers is beatiful!
+import sys
 from random import randrange
-number = randrange(100)
-print(number)
+# как мы, программисты любим, колорама! Ловите!
+import colorama
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+user_num = input(Back.GREEN + "Ограничьте подбор до целого числа, это число:")
+result = randrange(int(user_num))
+print(Back.BLUE + "Рандомное число:" + str(result) + "." + Back.BLUE)
